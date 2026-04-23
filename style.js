97a73df11b41bcf9219fd6951bfb6d6574361d20
piseth-4371 <pisethkhat4371@gmail.com>
2026-04-23 23:39:15 +0700

    responsive.2 and new pages update

--- style.js
+++ style.js
@@ -4,3 +4,24 @@ const menuBtn = document.querySelector('.icon-menu');
 menuBtn.addEventListener('click', () => {
     navLink.classList.toggle('active');
 });
+
+const bc = Array.from(document.querySelectorAll('.bc1, .bc2, .bc3, .bc4'));
+
+let stack = [...bc];
+
+function updateZindex(){
+    stack.forEach((el, index) =>{
+        el.style.zIndex = index + 1;
+    });
+}
+bc.forEach(box =>{
+    box.addEventListener('click', () => {
+        stack = stack.filter(el => el !== box);
+
+        stack.push(box);
+
+        updateZindex();
+    });
+});
+
+updateZindex();
